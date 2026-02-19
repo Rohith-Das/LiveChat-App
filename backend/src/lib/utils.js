dcba51efe9a6d8f2bda44815010fc57e1d2ac323
Rohit das <devrohith0@gmail.com>
2026-02-19 11:33:16 +0530

    Fix production cookie true

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -5,7 +5,7 @@ export const generateToken = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
     res.cookie("jwt", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
    sameSite: "none" ,
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
