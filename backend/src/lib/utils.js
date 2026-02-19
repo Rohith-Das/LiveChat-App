773829006c5fdd40efcac70909e89271c2619c49
Rohit das <devrohith0@gmail.com>
2026-02-19 12:07:11 +0530

    Fix cross-site cookie

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -3,12 +3,13 @@ import jwt from "jsonwebtoken";
 export const generateToken = (userId, res) => {
   try {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      secure: true,
-   sameSite: "none" ,
-      maxAge: 7 * 24 * 60 * 60 * 1000
-    });
+  res.cookie("jwt", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",   
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
+
   } catch (error) {
     console.log("Token generation error:", error);
     throw error; // Ensure error propagates
